c9444939cd836875abcc4bf20c31276d03aae652
andrehass <andre_hass@hotmail.com>
2017-12-11 00:07:02 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.alarmpassword = '1424'; // Alarm Password
+exports.wincom = 'COM5'; // Windows COM Port in case using Windows
+exports.linuxcom = '/dev/ttyUSB0'; // Linux Com Port in case using Linux
+exports.httpport = 3000; // NodeJS HTTP Port
+exports.baudRate = 9600; // Serial Speed port
+exports.shardlocation = "graph-na02-useast1.api.smartthings.com"; // SmartThing shard location
+exports.appid = '354a6698-b023-4132-b4a5-85d22116e15b'; // AppID you must get this value from the OAuth Authentication steps
+exports.accesstoken = '2fedd249-2e81-482d-b972-76e3f17e08e9'; // AccessToken you must get this value from the OAuth Authentication steps
+
