0741e6521f1cde5f0094bf1cfe5efefe9a6c9467
Hass <hasandre@f45c89abfd51.ant.amazon.com>
2017-12-23 16:41:58 -0600

    Adding Hours

--- hours.js
+++ hours.js
@@ -1,32 +1,28 @@
-var date = new Date();
+setAlarmDate();
 
-var minute = date.getMinutes().toString();
-if (minute.lenght == 1){
-   minute = "0"+minute;
-};
+function setAlarmDate() {
+    var date = new Date();
+    var minute = date.getMinutes().toString();
+        if (minute.lenght == 1){
+           minute = "0"+minute;
+            };
 
-var hour = date.getHours().toString();
-if (hour.lenght == 1){
-    hour = "0"+hour;
-};
+    var hour = date.getHours().toString();
+        if (hour.lenght == 1){
+        hour = "0"+hour;
+        };
+    var month = date.getMonth() + 1;
+        month = month.toString();
+        if(month.lenght == 1){
+          month = "0"+month;
+        };
+    var day = date.getDate();
+        if (day == 1){
+            day = "0"+day;
+        };
+    var year = date.getFullYear().toString().substring(2,4);
+    var cmd = minute + hour + month + day + year
+    cmd = "010"+cmd;
 
-
-var month = date.getMonth() + 1;
-month = month.toString();
-if(month.lenght == 1){
-    month = "0"+month;
-};
-
-
-var day = date.getDate();
-if (day == 1){
-    day = "0"+day;
+ console.log(cmd);
 }
-
-var year = date.getFullYear();
-
-var datetime = minute + hour.toString() + month + day + year.toString().substring(2,4);
-
-//day.toString() + ;
-
-console.log(datetime);
